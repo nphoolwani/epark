4417a11126d7c373f7fa20cbb4dcb4e0f43465e5
nphoolwani <58843617+nphoolwani@users.noreply.github.com>
2020-06-06 19:19:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = require('express')(); // Create an instance of an Express app
 var mobileApp = require('azure-mobile-apps')(); // Create an instance of a Mobile App with default settings
 
 mobileApp.tables.add('Users'); // Create a table for 'Users' with default settings
+mobileApp.tables.add('mall_details'); // Create a table for 'Mall Details' with default settings
 
 app.use(mobileApp);
 app.listen(process.env.PORT || 3000);
